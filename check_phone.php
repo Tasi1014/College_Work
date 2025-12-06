b90a000c6c3520ad9c7332152cb57206f5267909
Tashi Sherpa <sherpajack3@gmail.com>
2025-12-06 07:52:35 +0545

    modified the code of phone checker from xhr to fetch

--- check_phone.php
+++ check_phone.php
@@ -1,9 +1,11 @@
 <?php
 // Connect to database
 $conn = mysqli_connect("localhost", "root", "", "scripting_2025");
-
-// Get phone from GET request
-$phone = $_GET['phone'];
+if(!$conn){
+    die("Connection Failed: ". $conn_error());
+}
+if($_SERVER["REQUEST_METHOD"] == "GET"){
+    $phone = $_GET['phone'];
 
 // Query the database
 $query = "SELECT * FROM students WHERE phone = '$phone'";
@@ -15,4 +17,6 @@ if (mysqli_num_rows($result) > 0) {
 } else {
     echo "<span style='color:green;'>Phone number is unique</span>";
 }
+}
+
 ?>
